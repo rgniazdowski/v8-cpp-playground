69cfa2c4fe7a7c5455f7bd659eab1572f8bff464
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2022-03-05 03:24:19 +0100

    Initial version of unit test for bindings

--- tests/bindings.cpp
+++ tests/bindings.cpp
@@ -1,6 +1,99 @@
 #include <catch2/catch.hpp>
+#include <util/Tag.hpp>
 #include <util/Bindings.hpp>
+#include <resource/ManagedObject.hpp>
+//>---------------------------------------------------------------------------------------
+
+class TestExternal;
+using TagTestExternal = util::Tag<TestExternal>;
+using TestExternalHandle = util::Handle<TagTestExternal>;
+
+class TestExternal
+{
+
+}; //> TestExternal
+
+class TestClass;
+using TagTestClass = util::Tag<TestClass>;
+using TestClassHandle = util::Handle<TagTestClass>;
+
+class TestClass : public resource::ManagedObjectBase
+{
+    using handle_type = TestClassHandle;
+
+private:
+    std::string m_value;
+
+public:
+    float m_number;
+    int m_id;
+
+protected:
+    TestClassHandle m_handle;
+
+public:
+    TestClass(uint64_t handle, int id, float number, std::string_view value)
+        : m_handle(handle), m_id(id), m_number(number), m_value(value) {}
+
+public:
+    const std::string &getValue(void) const
+    {
+        return m_value;
+    }
+    void setValue(const std::string &value)
+    {
+        m_value = value;
+    }
+
+public:
+    handle_type const &getHandle(void) const
+    {
+        return m_handle;
+    }
+    inline util::HandleBase const &getHandleBase(void) const override { return m_handle; }
+}; //> TestClass
+//>---------------------------------------------------------------------------------------
+
+int TestGlobalFunction(int intValue, const std::string &stringValue)
+{
+    return intValue + 10 + (int)stringValue.length();
+}
+//>---------------------------------------------------------------------------------------
 
 TEST_CASE("Create simple bindings", "[bindings]")
 {
 }
+//!---------------------------------------------------------------------------------------
+
+TEST_CASE("Invoke function bindings manually", "[bindings]")
+{
+    auto pBinding = new util::BindInfoFunction("TestGlobalFunction", &TestGlobalFunction, {"intValue", "stringValue"});
+    util::WrappedValue::Args args = {util::WrappedValue::wrap(123), util::WrappedValue::wrap("Hello World!")};
+    int expectedResult = 123 + 10 + (int)strlen("Hello World!");
+
+    CHECK(pBinding->call(123, "Hello World!") == expectedResult);
+    CHECK(pBinding->callWithArgs(args) == expectedResult);
+
+    auto pWrappedResult = pBinding->callWrapped(args);
+    CHECK(pWrappedResult->get<int>() == expectedResult);
+    delete pWrappedResult;
+
+    delete pBinding;
+    util::reset_arguments(args);
+}
+//!---------------------------------------------------------------------------------------
+
+TEST_CASE("Invoke member method bindings manually", "[bindings]")
+{
+}
+//!---------------------------------------------------------------------------------------
+
+TEST_CASE("Create complete metadata bindings", "[bindings]")
+{
+}
+//!---------------------------------------------------------------------------------------
+
+TEST_CASE("Invoke bindings from metadata", "[bindings]")
+{
+}
+//!---------------------------------------------------------------------------------------
