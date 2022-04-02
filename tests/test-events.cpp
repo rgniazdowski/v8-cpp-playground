43f30a40c586d0e7e291a5311487a1e572b92881
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2022-04-02 12:24:36 +0200

    Small update  to 'events' unit test

--- tests/test-events.cpp
+++ tests/test-events.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch.hpp>
 
 #include <event/EventManager.hpp>
+#include <resource/GlobalObjectRegistry.hpp>
 
 event::EventManager *g_eventMgr = nullptr;
 
@@ -24,15 +25,48 @@ void destroyEventManager(void)
 TEST_CASE("Initialize event manager II", "[events]")
 {
     auto pEventMgr = initializeEventManager();
+    destroyEventManager();
 }
 //!---------------------------------------------------------------------------------------
 
+bool TestCallbackA(event::EventCombined *event)
+{
+    return true;
+}
+
+class TestClass
+{
+public:
+    bool TestMemberCallbackA(event::EventCombined *event)
+    {
+        return true;
+    }
+};
+
 TEST_CASE("Register callbacks for standard events", "[events]")
 {
+    auto testObject = new TestClass();
+    auto pEventMgr = initializeEventManager();
+    pEventMgr->addCallback(event::Type::ProgramInit, &TestCallbackA);
+    pEventMgr->addCallback(event::Type::ProgramInit, &TestClass::TestMemberCallbackA, testObject);
+
+    pEventMgr->isRegistered(testObject);
+    pEventMgr->isRegistered(&TestCallbackA);
+
+    pEventMgr->isRegistered(&TestClass::TestMemberCallbackA, (TestClass *)nullptr);
+    pEventMgr->isRegistered(&TestClass::TestMemberCallbackA, testObject);
+
+    pEventMgr->isRegistered(event::Type::ProgramInit, testObject);
+    pEventMgr->isRegistered(event::Type::ProgramInit, &TestCallbackA);
+    pEventMgr->isRegistered(event::Type::ProgramInit, &TestClass::TestMemberCallbackA, (TestClass *)nullptr);
+    pEventMgr->isRegistered(event::Type::ProgramInit, &TestClass::TestMemberCallbackA, testObject);
+    destroyEventManager();
 }
 //!---------------------------------------------------------------------------------------
 
 TEST_CASE("Trigger events in fake loop", "[events]")
 {
+    auto pEventMgr = initializeEventManager();
+    destroyEventManager();
 }
 //!---------------------------------------------------------------------------------------
